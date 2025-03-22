8fcc96a29c04bdb1449b83fb59d3771de852c47c
Aldhi <aldhi.albadri@gmail.com>
2025-03-22 16:50:40 +0700

    fix .env

--- resources/views/components/auth_layout.blade.php
+++ resources/views/components/auth_layout.blade.php
@@ -9,7 +9,7 @@
         <meta content="{{ config('app.app_desc') }}" name="description" />
         <meta content="{{ config('app.app_author') }}" name="author" />
         <!-- App favicon -->
-        <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico')}}">
+        <link rel="shortcut icon" href="{{ asset('assets/images/favicon.png')}}">
 
         <!-- Bootstrap Css -->
         <link href="{{ asset('assets/css/bootstrap.min.css')}}" id="bootstrap-style" rel="stylesheet" type="text/css" />
@@ -35,7 +35,7 @@
                                         <div>
                                             <div class="text-center">
                                                 <div>
-                                                    <a href="index.html" class="logo"><img src="{{ asset('assets/images/logo.png') }}" height="50" alt="logo"></a>
+                                                    <a href="index.html" class="logo"><img src="{{ asset('assets/images/logo-dark.png') }}" height="50" alt="logo"></a>
                                                 </div>
 
                                                 <h4 class="font-size-18 mt-4">{{ config('app.name') }}</h4>
