c47f0f9b7054681551a138a5cd2daaa1d26aef2a
Aldhi <aldhi.albadri@gmail.com>
2025-06-20 13:58:37 +0700

    fix

--- resources/views/dashboard/insight.blade.php
+++ resources/views/dashboard/insight.blade.php
@@ -1,5 +1,8 @@
 <div>
-    <canvas id="confetti-canvas" style="position:fixed; top:0; left:0; width:100%; height:100%; pointer-events:none; z-index:9999;"></canvas>
+    <canvas id="confetti-canvas"
+        style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; z-index: 9999; pointer-events: none;">
+    </canvas>
+
     @include('dashboard.inc.rank1')
 
 
