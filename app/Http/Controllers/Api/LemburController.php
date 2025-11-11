81d8fdc0873cc3fa0a54d20b01a3cb7ffd212b37
Aldhi <aldhi.albadri@gmail.com>
2025-11-11 07:18:34 +0700

    fix lembur FE BE

--- app/Http/Controllers/Api/LemburController.php
+++ app/Http/Controllers/Api/LemburController.php
@@ -68,11 +68,60 @@ class LemburController extends Controller
 
     }
 
+    public function update($id, Request $r)
+    {
+
+        $validated = $r->validate([
+            "data_employee_id" => "required",
+            "from" => "required|date",
+            "to" => "required|date|after_or_equal:from",
+        ], [
+            'data_employee_id.required' => 'ID karyawan wajib diisi.',
+            'from.required' => 'Tanggal mulai wajib diisi.',
+            'from.date' => 'Tanggal mulai wajib diisi dengan benar.',
+            'to.required' => 'Tanggal selesai wajib diisi.',
+            'to.date' => 'Tanggal selesai wajib diisi dengan benar.',
+            'to.after_or_equal' => 'Tanggal selesai tidak boleh lebih awal dari tanggal mulai.',
+        ]);
+
+        $startTime = Carbon::parse($validated['from'].':00');
+        $endTime   = Carbon::parse($validated['to'].':00');
+        $tanggal = $startTime->toDateString();
+
+        $data = [
+            'id' => $id,
+            'form' => [
+                'data_employee_id'   => $validated['data_employee_id'],
+                'approved_by' => $this->dataEmployeeRepo->pengawasCheck($validated['data_employee_id']),
+                'tanggal' => $tanggal,
+                'checkin_time_lembur' => $startTime->copy()->subHour()->format('Y-m-d H:i:s'),
+                'work_time_lembur' => $startTime->format('Y-m-d H:i:s'),
+                'checkin_deadline_time_lembur' => $startTime->copy()->addMinutes(15)->format('Y-m-d H:i:s'),
+                'checkout_time_lembur' => $endTime->format('Y-m-d H:i:s'),
+                'checkout_deadline_time_lembur' => $endTime->copy()->addHours(2)->format('Y-m-d H:i:s'),
+                'status' => 'Proses'
+            ]
+        ];
+
+        if($this->dataLemburRepo->update($data)){
+            return response()->json([
+                'message' => 'Data lembur berhasil diubah.',
+                'data'    => $data,
+            ], 201);
+        }else{
+            return response()->json([
+                'message' => 'Terjadi masalah ketika menyimpan data.',
+                'data'    => $data,
+            ], 400);
+        }
+
+    }
+
     public function delete($id)
     {
         if($this->dataLemburRepo->delete($id)){
             return response()->json([
-                'message' => 'Data izin berhasil dihapus.',
+                'message' => 'Data lembur berhasil dihapus.',
                 'data'    => $id,
             ], 201);
         }else{
